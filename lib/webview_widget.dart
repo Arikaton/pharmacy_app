f49f5f2eb1f67adf94d25c9673ec6df51ce25502
Mikle Top <mtop.sib@gmail.com>
2020-06-09 01:48:47 +0700

    debag lines

--- lib/webview_widget.dart
+++ lib/webview_widget.dart
@@ -28,6 +28,13 @@ class _WebViewWidgetState extends State<WebViewWidget>{
     _onUrlChanged = flutterWebviewPlugin.onUrlChanged.listen((String url) {
       var uri = Uri.parse(url);
       var query = uri.queryParameters;
+      print('uri.queryParameters:');
+      print(uri.queryParameters);
+      print('uri:');
+      print(uri);
+      print('url:');
+      print(url);
+
       if (query["code"] != null){
         postEsiaAndClose(query['code'], query['state']);
       }
@@ -57,10 +64,13 @@ class _WebViewWidgetState extends State<WebViewWidget>{
 
   void postEsiaAndClose(String code, String state) async {
     bool success;
+    print('Ща будем отправлять на сервак всякое');
     if (widget.url.length == 1){
+      print('шлем на сервак из профиля');
       success = await ServerLogin.postDataFromEsia(code, state);
     } else {
       success = await ServerLogin.postDataFromEsia(code, state, userID: widget.url[1]);
+      print('Шлем на сервак из родствеников');
     }
     if (!success){
       _showSnackBar(context);
@@ -76,7 +86,7 @@ class _WebViewWidgetState extends State<WebViewWidget>{
 
   void _showSnackBar(BuildContext context){
     var snackBar = SnackBar(
-        content: Text("Сервер авторизации Госуслуги временно не доступен"),
+        content: Text("Сервер авторизации Госуслуги временно не доступен."),
         action: SnackBarAction(label: "ок", onPressed: () {},)
     );
     Scaffold.of(context).showSnackBar(snackBar);
